b9957b0409c48a28f1f62c9690ef3730ef86a455
Punit Subudhi <punitsubudhi@gmail.com>
2024-10-16 16:58:22 +0100

    .

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 uri = f"mongodb+srv://{os.getenv('db_username')}:{os.getenv('db_password')}@{os.getenv('mongo_uri')}/?retryWrites=true&w=majority&appName=tasks"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['forms']
+
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -22,8 +23,13 @@ def root_dir():
     return os.path.abspath(os.path.dirname(__file__))
 
 def send_push_notification(notification_data):
-    url = f"https://ntfy.sh/{os.getenv('ntfy_topic')}"
-    rq.post(url, data=notification_data)
+    try:
+        url = f"https://ntfy.sh/{os.getenv('ntfy_topic')}"
+        resp = rq.post(url, data=notification_data)
+        return json.dumps(resp.json())
+    except Exception as e:
+        db['errors'].insert_one({"error": str(e)})
+        return str(e)
 
 def process_data(data):
     processed_data = {}
@@ -46,14 +52,17 @@ def home_page():
     if request.headers.get('type') is None:
         # return the last 10 documents from the database according to timestamp
         #return f"{list(db['forms'].find().sort('timestamp', 1).limit(10))}"
-        return redirect("https://solarcventures.com", code=302)
+        return redirect(os.getenv("redirect_url"), code=302)
     elif request.headers.get('type') == 'form':
         # get the form data
         form_data = request.json
         # push raw data to the database
         result = db['raw'].insert_one(form_data)
         
-        send_push_notification(f"New form submission from {form_data['filledby']}\nForms Data: \n{(form_data)}")
+        if form_data['filledby'] is not None:
+            send_push_notification(f"New form submission from {form_data['filledby']}\nForms Data: \n{(form_data)}")
+        else:
+            send_push_notification(f"New form submission\nForms Data: \n{(form_data)}")    
         
         # process the data
         processed_data = process_data(form_data)
@@ -62,9 +71,11 @@ def home_page():
         
         return f"{result}"
     elif request.headers.get('type') == 'push':
-        notification_data = request.json
-        send_push_notification(notification_data)
-        return "Pushed notification"
+        notification_data = request.data
+        response = send_push_notification(notification_data)
+        return response
+    elif request.headers.get('type') == 'get':
+        return f"{list(db['raw'].find().sort('timestamp', 1).limit(10))}"
 
 @app.route("/forms", methods=['GET'])
 def get_forms():
