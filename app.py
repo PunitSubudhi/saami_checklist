828a7f3f8ff4adc7e3f04c138a0ce450a7cb1fc2
Punit Subudhi <punitsubudhi@gmail.com>
2024-10-16 16:26:23 +0100

    default route changes and testing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from flask import Flask,request
+from flask import Flask, request, render_template, redirect
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import time
@@ -18,17 +18,17 @@ try:
 except Exception as e:
     print(e)
 
+def root_dir():
+    return os.path.abspath(os.path.dirname(__file__))
+
 def send_push_notification(notification_data):
     url = f"https://ntfy.sh/{os.getenv('ntfy_topic')}"
-    rq.post(url, json=notification_data)
+    rq.post(url, data=notification_data)
 
 def process_data(data):
     processed_data = {}
     processed_data['timestamp'] = time.time()
-    
-    
     return processed_data
-
 
 '''
 api_tokens = ZohoAPITokens(
@@ -39,28 +39,36 @@ api_tokens = ZohoAPITokens(
 )
 '''
 
-@app.route("/",methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def home_page():
-    #check if header has "type:form"
-    #if it does, then it is a form submission
+    # check if header has "type:form"
+    # if it does, then it is a form submission
     if request.headers.get('type') is None:
-        #return the last 10 documents from the database according to timestamp
-        return f"{list(db['forms'].find().sort('timestamp',1).limit(10))}"
-    if request.headers.get('type') == 'form':
-        #get the form data
+        # return the last 10 documents from the database according to timestamp
+        #return f"{list(db['forms'].find().sort('timestamp', 1).limit(10))}"
+        return redirect("https://solarcventures.com", code=302)
+    elif request.headers.get('type') == 'form':
+        # get the form data
         form_data = request.json
-        #push raw data to the database
+        # push raw data to the database
         result = db['raw'].insert_one(form_data)
         
         send_push_notification(f"New form submission from {form_data['filledby']}\nForms Data: \n{(form_data)}")
         
-        #process the data
+        # process the data
         processed_data = process_data(form_data)
-        #insert the form data to the database
+        # insert the form data to the database
         result = db['processed'].insert_one(processed_data)
         
         return f"{result}"
-    
-    
+    elif request.headers.get('type') == 'push':
+        notification_data = request.json
+        send_push_notification(notification_data)
+        return "Pushed notification"
+
+@app.route("/forms", methods=['GET'])
+def get_forms():
+    return f"{list(db['raw'].find().sort('timestamp', 1).limit(10))}"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
